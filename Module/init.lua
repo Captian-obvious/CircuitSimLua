5117e4df6ae5d3c9a898322752e4ac74d8fc1e90
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2024-01-17 16:35:07 -0500

    Update init.lua

--- Module/init.lua
+++ Module/init.lua
@@ -12,4 +12,12 @@ function cs.loadDependencies(self)
     
 end
 
+function cs.loadScript(self,path,onsuc,onerr)
+    if (path~=nil) then
+        local suc,res=pcall(function()
+            return require(path)
+        end)
+    end
+end
+
 return cs
