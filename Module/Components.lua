681bc618b2733e46206a58f8e5326c1272d49275
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2024-01-17 16:24:21 -0500

    Update Components.lua

--- Module/Components.lua
+++ Module/Components.lua
@@ -3,6 +3,13 @@ local comp={}
 function comp.Init(self)
     if self.isInitialized~=true then
         self.isInitialized=true
+        self.rules=self.deps.circuitSimulationRules.new()
+    end
+end
+
+function comp.handleSimulation(self,componentTable)
+    if (self.rules~=nil) then
+        --will implement later.
     end
 end
 
